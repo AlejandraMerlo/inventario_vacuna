37d7c93d921e4ddbe8f3df053860c55cc8094252
AlejandraMerlo <alejandra.merloa@outlook.com>
2023-02-25 02:48:17 -0500

    Creado archivo repositorio de modulo Usuario, agregadas consultas.

---
+++ src/main/java/com/inventario/modules/usuarios/repository/UsuarioRepository.java
@@ -0,0 +1,21 @@
+package com.inventario.modules.usuarios.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.inventario.modules.models.Usuario;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+
+	@Query(value = "SELECT usuario FROM Usuario usuario ORDER BY usuario ASC")
+	public List<Usuario> getUsuarios();
+
+	@Query(value = "SELECT usuario FROM Usuario usuario WHERE usuario.codigoUsuario = :codigoUsuario")
+	public Usuario getUsuarioByCodigo(@Param("codigoUsuario") int codigoUsuario);
+
+	@Query(value = "SELECT usuario FROM Usuario usuario WHERE usuario.usuario = :nombreUsuario ")
+	public Usuario getUsuarioByNombre(@Param("nombreUsuario") String nombeUsuario);
+}
