f50c7f6abee780f11630d1fb2d8bba660460456e
AlejandraMerlo <alejandra.merloa@outlook.com>
2023-02-26 13:57:36 -0500

    Agregadas configuraciones para manejo de swagger en el proyecto.

--- src/main/java/com/inventario/InventarioVacunasApplication.java
+++ src/main/java/com/inventario/InventarioVacunasApplication.java
@@ -2,8 +2,14 @@ package com.inventario;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-@SpringBootApplication
+@EnableWebMvc
+@EnableTransactionManagement
+@SpringBootApplication(scanBasePackages = { "com.inventario.modules.*.*.controller",
+		"com.inventario.modules.*.controller", "com.inventario.modules.*.*.service", "com.inventario.modules.*.service",
+		"com.inventario.configurations" })
 public class InventarioVacunasApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/inventario/configurations/WebConfig.java
@@ -0,0 +1,22 @@
+package com.inventario.configurations;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/**", "/webjars/**", "/img/**", "/css/**", "/js/**", "swagger-ui.html")
+				.addResourceLocations("classpath:/META-INF/resources/webjars/", "classpath:/META-INF/resources/");
+	}
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**").allowedOrigins("*").allowedHeaders("*").allowedMethods("*");
+	}
+
+}
